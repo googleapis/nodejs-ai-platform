a530ca27d99d4e720135f3f671d58bead39df8ce
Eric Schmidt <erschmid@google.com>
2021-02-16 12:23:37 -0800

    fix: generates protos.js and other artifacts

--- protos/protos.d.ts
+++ protos/protos.d.ts
@@ -59739,8 +59739,7 @@ export namespace google {
             REQUIRED = 2,
             OUTPUT_ONLY = 3,
             INPUT_ONLY = 4,
-            IMMUTABLE = 5,
-            UNORDERED_LIST = 6
+            IMMUTABLE = 5
         }
 
         /** Properties of a ResourceDescriptor. */
@@ -59763,9 +59762,6 @@ export namespace google {
 
             /** ResourceDescriptor singular */
             singular?: (string|null);
-
-            /** ResourceDescriptor style */
-            style?: (google.api.ResourceDescriptor.Style[]|null);
         }
 
         /** Represents a ResourceDescriptor. */
@@ -59794,9 +59790,6 @@ export namespace google {
 
             /** ResourceDescriptor singular. */
             public singular: string;
-
-            /** ResourceDescriptor style. */
-            public style: google.api.ResourceDescriptor.Style[];
 
             /**
              * Creates a new ResourceDescriptor instance using the specified properties.
@@ -59876,12 +59869,6 @@ export namespace google {
                 HISTORY_UNSPECIFIED = 0,
                 ORIGINALLY_SINGLE_PATTERN = 1,
                 FUTURE_MULTI_PATTERN = 2
-            }
-
-            /** Style enum. */
-            enum Style {
-                STYLE_UNSPECIFIED = 0,
-                DECLARATIVE_FRIENDLY = 1
             }
         }
 
