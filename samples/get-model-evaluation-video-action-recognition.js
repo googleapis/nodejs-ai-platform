917af1d33087279340463c5ebf6adbdfd2b9a749
Eric Schmidt <erschmid@google.com>
2021-05-13 15:25:04 -0700

    samples: more samples

--- samples/get-model-evaluation-video-action-recognition.js
+++ samples/get-model-evaluation-video-action-recognition.js
@@ -54,15 +54,15 @@ async function main(modelId, evaluationId, project, location = 'us-central1') {
     // Create get model evaluation request
     const [response] = await modelServiceClient.getModelEvaluation(request);
 
-    console.log('Get model evaluation video classification response');
+    console.log('Get model evaluation video action recognition response');
     console.log(`\tName : ${response.name}`);
     console.log(`\tMetrics schema uri : ${response.metricsSchemaUri}`);
     console.log(`\tMetrics : ${JSON.stringify(response.metrics)}`);
     console.log(`\tCreate time : ${JSON.stringify(response.createTime)}`);
     console.log(`\tSlice dimensions : ${response.sliceDimensions}`);
   }
-  getModelEvaluationVideoClassification();
-  // [END aiplatform_get_model_evaluation_video_classification]
+  getModelEvaluationVideoActionRecognition();
+  // [END aiplatform_get_model_evaluation_video_action_recognition]
 }
 
 process.on('unhandledRejection', err => {
